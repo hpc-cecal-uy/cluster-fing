2a0173dbb8bfaebd5d6a3b4f4c4a863cd2c3f984
Santiago Iturriaga <santiago.iturriaga@gmail.com>
2014-11-27 08:16:03 -0200

    Actualización de la configuración y comentarios en scripts con ejemplos de reservas

--- ejemplos-setres/reserva_24cores_node40.sh
+++ ejemplos-setres/reserva_24cores_node40.sh
@@ -1,1 +1,5 @@
+#
+# Reserva de 24 cores en node40 a nombre del usuario algevo02
+# Efectiva desde el momento en que se ejecuta el comando hasta el 03/11 a las 23:50
+#
 setres -u algevo02 -e 23:59:00_11/03 -r PROCS=24 'node40'
--- ejemplos-setres/reserva_total_por_mantenimiento.sh
+++ ejemplos-setres/reserva_total_por_mantenimiento.sh
@@ -1,1 +1,5 @@
+#
+# Reserva de todo el sistema por mantenimiento
+# Efectiva desde el 03/09 a las 06:00 hasta el 03/09 a las 07:00
+#
 setres -s 06:00:00_09/03 -e 07:00:00_09/03 ALL
